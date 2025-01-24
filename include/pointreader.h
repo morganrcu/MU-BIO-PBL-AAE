dea021f503be8a88013baba3d64404c45e6d11b8
JuneSenar <june.senar@alumni.mondragon.edu>
2025-01-24 08:40:38 +0100

    Calculador de max, Velocidad y pointreader hechos, falta liberar array y tests

--- include/pointreader.h
+++ include/pointreader.h
@@ -1,3 +1,5 @@
 #pragma once
 #include "point.h"
-int readPoints(const char* file, point_t** points); 
+int readPoints(const char* file, point_t** points);
+int read_csv_header();
+struct point_t parse_csv_row(char* fila[]);
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,57 @@
-#include <csv.h>
 #include <stdio.h>
 #include "point.h"
 #include "pointreader.h"
 #include "angularvelocitycalculator.h"
 
-int main(int argc, char ** argv) {
+int main(int argc, char** argv) {
+	/*
+		ARGUMENTOS:
+
+		int main(int argc, char *argv[]);
+
+			argc (argument count):
+				Es un entero que indica el número de argumentos pasados al programa, incluyendo el nombre del programa en sí.
+				Siempre es al menos 1 porque el primer argumento (argv[0]) es el nombre del ejecutable.
+
+			argv (argument vector):
+				Es un array de punteros a cadenas (char *) que contiene los argumentos pasados al programa.
+
+				- argv[0] es el nombre (o ruta) del programa.
+				- argv[1] hasta argv[argc-1] son los argumentos adicionales.
+				- argv[argc] es un puntero nulo (NULL), marcando el final del array.
+	*/
+
 	if (argc != 2) {
 		printf("Program to compute maximum angular velocity from a txt file of comma separated values produced by Tracker\n");
 		printf("Usage: %s <filename>\n", argv[0]);
+
 		return -1;
 	}
-	struct point_t* points;
+
+	struct point_t* points = NULL;
 
 	int numberOfPoints = readPoints(argv[1], &points);
 
-	if (numberOfPoints < 0) {
-		printf("Error reading file\n");
+	if (numberOfPoints < 2) {
+		printf("Error: no hay suficientes puntos");
 		return -1;
 	}
-	float maximumAngularVelocity = computeMaximumAngularVelocity(points, numberOfPoints);
-	
-	printf("Maximum angular velocity: %f\n", maximumAngularVelocity);
 
-	freePoints(&points);
+	float maximumAngularVelocity = computeMaximumAngularVelocity(points, numberOfPoints);
+
+	printf("Maximum angular velocity: %.20f  [rad/s]\n", maximumAngularVelocity);
+
+	/*
+	* Lo de abajo es una verificacion de que se han leido bien los puntos. Luego se puede borrar.
+	*/
+
+	printf("Num puntos: %d\n\n", numberOfPoints);
+
+	for (int i = 0; i < numberOfPoints; i++) {
+		printf("Point %d: t=%.20f, x=%.20f, y=%.20f\n", i, points[i].t, points[i].x, points[i].y);
+	}
+
+	freePoints(&points, numberOfPoints );
+
 	return 0;
 }
