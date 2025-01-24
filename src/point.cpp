9a17a3b83025dfa30bcd05580db0c86190f92e45
JuneSenar <june.senar@alumni.mondragon.edu>
2025-01-24 10:04:34 +0100

    freepoints

--- src/point.cpp
+++ src/point.cpp
@@ -6,15 +6,13 @@
     //La función  debe liberar la memoria asignada tanto para cada elemento del arreglo como para el propio arreglo. 
 	
 void freePoints(point_t** points, int numberOfPoints) {
-	
-   /*
-        // Comprobamos que el puntero no sea NULL
-        if (points != NULL && *points != NULL) {
+
+        if (points != NULL) {
             // Liberamos la memoria del array de structs
             free(*points);
             printf(".");
             // Ponemos el puntero original a NULL para evitar el uso posterior de memoria liberada
             *points = NULL;
         }
-*/
+
 }
