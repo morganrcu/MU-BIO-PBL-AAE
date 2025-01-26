3c7eb16f6bbb0bfc02842678e3e0772f117a8c6a
MaiderB <maiderbeitiaa@gmail.com>
2025-01-26 20:36:49 +0100

    Update point.h
    Kodigoaren struct funtzionamendua azaldu da, non bi aldagaiak kontuan hartzen diren

--- include/point.h
+++ include/point.h
@@ -1,7 +1,22 @@
 #pragma once
 typedef struct point_t {
-	//TODO 1. Decide what needs to be saved in point_t struct
-	int none; //remove this line, jus to avoid compilation error
+	float t;// denbora (segundutan)
+	float angle; // theta angelua
 } point_t;
 
-void freePoints(point_t** points);
+
+void freePoints(point_t** points);//point_t array-ari esleitutako memoria askatuko duen funtzio bat deklaratu.
+
+
+// KODIGOAREN FUNTZIOA: point_t egitura definitzea
+// 
+// AZALPEN GEHIGARRIAK:
+// 
+// struct bat datu ezberdinak "entitate" bakar batean biltzen ditu, nolabait. 
+// Oso erabilgarria da zenbait propietate erlazionatu duen zerbait adierazteko. 
+// Adibidez, plano bateko puntu zehatz bat adierazteko, bere posizio angeluarra
+// eta puntu horri dagokion denbora biltzen dituen struct-a erabili daiteke 
+// (point_t hori egiten du).
+// 
+// Typedef erabiliz,point_t kodigoan gehitu daiteke  
+// struct-a lehenik idaztzi barik; hala kodea sinplifikatuz. 
