381fae49f34a35afab3c171f37df9f524073864a
Mikel Otaegui <otaeguimikel@gmail.com>
2025-01-27 09:28:59 +0100

    Abiadura angeluarraren kalkuluak egin ditut. Orain puntuak irakurtzeko kodigoa idatzi eta baloreak izendatu behar dira.

--- src/agularvelocitycalculator.cpp
+++ src/agularvelocitycalculator.cpp
@@ -1,6 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio> // Para usar printf
+#include "point.h"
 #include "angularvelocitycalculator.h"
+#include <math.h> // Para fabs
 
 float computeMaximumAngularVelocity(const point_t* points, int numberOfPoints) {
-	//TODO 3. Compute the maximum angular velocity
-	return 0;
+    if (numberOfPoints < 2) {
+        printf("Not enough points to calculate angular velocity\n");
+        return 0.0f;
+    }
+
+    float maxAngularVelocity = 0.0f;
+
+    for (int i = 1; i < numberOfPoints; i++) {
+        float deltaTheta = points[i].theta - points[i - 1].theta;
+        float deltaTime = points[i].t - points[i - 1].t;
+
+
+        if (deltaTime != 0) {
+            float angularVelocity = fabs(deltaTheta / deltaTime);
+
+
+            if (angularVelocity > maxAngularVelocity) {
+                maxAngularVelocity = angularVelocity;
+            }
+        }
+        else {
+            printf("Skipped division by zero at index %d\n", i);
+        }
+    }
+
+
+    return maxAngularVelocity;
 }
